187ab8c71ff55a97a20a55a1bc61dc9a5e0e4bd9
Ingrid sura <sura.ingrid@gmail.com>
2019-11-15 03:15:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,3 +1,8 @@
+birthday_kids = {
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+}
 # birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
@@ -5,7 +10,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  
 end
 
 
